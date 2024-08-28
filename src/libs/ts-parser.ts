bd376be82b06bb74a508c6d127f967d5bcac216a
唐小虎 <1731050423@qq.com>
2024-08-28 09:03:35 +0800

    远程函数增加文件命名空间

--- src/libs/ts-parser.ts
+++ src/libs/ts-parser.ts
@@ -1,91 +1,189 @@
-import fs from 'fs'
-import path from 'path'
-import ts from 'typescript'
+import fs from "fs";
+import path from "path";
+import ts from "typescript";
 import {
   Extractor,
   ExtractorConfig,
   ExtractorResult,
-} from '@microsoft/api-extractor'
-import { RPCApi, RPCNsApi } from './templates'
-import generator from 'dts-generator'
+} from "@microsoft/api-extractor";
+import { RPCApi, RPCNsApi } from "./templates";
+import generator from "dts-generator";
 
 export const tsParse = async (args: {
-  baseDir: string
-  out: string // Output path(including file name)
-  files: string[] //
-}) => await generator(args)
+  baseDir: string;
+  out: string; // Output path(including file name)
+  files: string[]; //
+}) => await generator(args);
 
 // Generate remote function frontend proxy
 // RPCDir: absolute path of the root directory of the remote function
 export async function RPCProxy(serverName: string, RPCDir: string) {
-  const RPCTmpdts = `__RPCTmp.d.ts`
   // Create index.ts file for all functions
-  createIndex(RPCDir)
-  // Generate dts temporary file for index.ts
-  await tsParse({ baseDir: RPCDir, out: RPCTmpdts, files: ['index.ts'] })
-  // Generate frontend function based on dts file
-  const api = generate(RPCTmpdts)
-  // Delete dts temporary file
-  try {
-    fs.rmSync(RPCTmpdts)
-  } catch (e) {}
+  createIndex(RPCDir);
+  // // Generate dts temporary file for index.ts
+  // await tsParse({ baseDir: RPCDir, out: RPCTmpdts, files: ["index.ts"] });
+  // // Generate frontend function based on dts file
+  // const api = generate(RPCTmpdts);
+  const api = await createClientRpc(RPCDir);
 
-  return api
+  // // Delete dts temporary file
+  // try {
+  //   fs.rmSync(RPCTmpdts);
+  // } catch (e) {}
 
+  return api;
+
+  async function createClientRpc(root: string) {
+    const exps = {};
+    const rootIdx = path.join(root, "index.ts");
+    const Iter = (d: string) => {
+      if (fs.statSync(d).isDirectory()) {
+        fs.readdirSync(d, "utf-8").forEach((v) => Iter(path.join(d, v)));
+      } else {
+        if (d.endsWith("ts") && d !== rootIdx) {
+          const file_name = d.replaceAll("\\", "/").slice(root.length + 1, -3);
+          file_name.split("/").reduce((_, v, k) => {
+            if (!_[v]) {
+              _[v] = {};
+            }
+            if (k === file_name.split("/").length - 1) {
+              _[v] = d;
+            }
+            return _[v];
+          }, exps);
+        }
+      }
+    };
+    Iter(root);
+    const geneExp = async (v) => {
+      if (typeof v === "string") {
+        const RPCTmpdts = `__RPCTmp.d.ts`;
+        // Generate dts temporary file for index.ts
+        await tsParse({ baseDir: root, out: RPCTmpdts, files: [v] });
+        // Generate frontend function based on dts file
+        const file_name = v.replaceAll("\\", "/").slice(root.length + 1, -3);
+        const api = generate(RPCTmpdts, file_name.split("/").map(d => `'${d}'`));
+
+        // Delete dts temporary file
+        try {
+          fs.rmSync(RPCTmpdts);
+        } catch (e) {}
+        return api;
+      } else {
+        return await generateE(v);
+      }
+    };
+    const generateE = async (obj) => {
+      let apit = "";
+      for (const key in obj) {
+        const t = await geneExp(obj[key]);
+        apit += `export namespace ${key} {\n ${t} \n}`;
+      }
+      return apit;
+    };
+
+    return await generateE(exps);
+  }
   function createIndex(root: string) {
-    const exps: string[] = []
-    const rootIdx = path.join(root, 'index.ts')
-    let tmpIdx = ''
+    const imports: string[] = [];
+    const exports = {};
+    const rootIdx = path.join(root, "index.ts");
+    let tmpIdx = "";
     if (fs.existsSync(rootIdx)) {
-      tmpIdx = fs.readFileSync(rootIdx, 'utf-8')
-      fs.rmSync(rootIdx)
+      tmpIdx = fs.readFileSync(rootIdx, "utf-8");
+      fs.rmSync(rootIdx);
     }
     const Iter = (d: string) => {
       if (fs.statSync(d).isDirectory()) {
-        fs.readdirSync(d, 'utf-8').forEach((v) => Iter(path.join(d, v)))
+        fs.readdirSync(d, "utf-8").forEach((v) => Iter(path.join(d, v)));
       } else {
-        exps.push(`export * from './${d.replaceAll("\\", "/").slice(root.length + 1, -3)}'`)
+        if (d.endsWith("ts")) {
+          const file_name = d.replaceAll("\\", "/").slice(root.length + 1, -3);
+          imports.push(
+            `import * as I_${file_name.replaceAll("/", "_")} from './${d
+              .replaceAll("\\", "/")
+              .slice(root.length + 1, -3)}'`
+          );
+          file_name.split("/").reduce((_, v, k) => {
+            if (!_[v]) {
+              _[v] = {};
+            }
+            if (k === file_name.split("/").length - 1) {
+              _[v] = `I_${file_name.replaceAll("/", "_")}`;
+            }
+            return _[v];
+          }, exports);
+        }
       }
-    }
-    Iter(root)
-    const content = exps.length ? exps.join('\n') : tmpIdx
-    fs.writeFileSync(rootIdx, content, 'utf-8')
+    };
+    Iter(root);
+    const geneExp = (v) => {
+      if (typeof v === "string") {
+        return v;
+      } else {
+        return generateE(v);
+      }
+    };
+    const generateE = (obj) => {
+      let tt = "{\n";
+      for (const key in obj) {
+        const t = geneExp(obj[key]);
+        tt += `${key}: ${t}, \n`;
+      }
+      tt += "}\n";
+      return tt;
+    };
+
+    const impcontent = imports.length ? imports.join("\n") : tmpIdx;
+    const expcontent = Object.keys(exports)
+      ? Object.keys(exports)
+          .map(
+            (v) =>
+              `export const ${v} = ${
+                typeof exports[v] === "string"
+                  ? exports[v]
+                  : generateE(exports[v])
+              }`
+          )
+          .join("\n")
+      : "";
+    fs.writeFileSync(rootIdx, impcontent + "\n" + expcontent, "utf-8");
   }
 
-  function generate(dtsFile: string) {
-    let rtn = ''
-    const tsContent = fs.readFileSync(path.resolve(dtsFile), 'utf-8')
+  function generate(dtsFile: string, dns: string[]) {
+    let rtn = "";
+    const tsContent = fs.readFileSync(path.resolve(dtsFile), "utf-8");
     const sourceFile = ts.createSourceFile(
-      '',
+      "",
       tsContent,
       ts.ScriptTarget.Latest
-    )
+    );
     const moduleParse = (
       sub: ts.ModuleBlock | ts.ModuleBody,
       ns = <string[]>[]
     ) => {
-      let rtn2 = '',
-        Nd
+      let rtn2 = "",
+        Nd;
       sub.forEachChild((node) => {
-        let name = ''
+        let name = "";
         switch (node.kind) {
           case ts.SyntaxKind.VariableStatement:
           case ts.SyntaxKind.TypeAliasDeclaration:
-            rtn2 += '\n  ' + tsContent.slice(node.pos, node.end)
-            break
+            rtn2 += "\n  " + tsContent.slice(node.pos, node.end);
+            break;
           case ts.SyntaxKind.ExportDeclaration:
-            break
+            break;
           case ts.SyntaxKind.ModuleDeclaration:
-            Nd = <ts.ModuleDeclaration>node
-            name = Nd.name.text
+            Nd = <ts.ModuleDeclaration>node;
+            name = Nd.name.text;
             rtn2 += RPCNsApi(
               name,
               moduleParse(<ts.ModuleBlock>Nd.body, [...ns, `'${name}'`])
-            )
-            break
+            );
+            break;
           case ts.SyntaxKind.FunctionDeclaration:
-            Nd = <ts.FunctionDeclaration>node
-            name = Nd.name!.text
+            Nd = <ts.FunctionDeclaration>node;
+            name = Nd.name!.text;
             const args = {
               params: tsContent
                 .slice(
@@ -93,48 +191,48 @@ export async function RPCProxy(serverName: string, RPCDir: string) {
                   Nd.parameters.slice(-1)[0].end
                 )
                 .trim(),
-              rtnType: '',
+              rtnType: "",
               call: <string[]>[],
-            }
+            };
 
             args.rtnType = Nd.type
               ? tsContent.slice(Nd.type.pos, Nd.type.end).trim()
-              : 'void'
+              : "void";
 
             args.call = Nd.parameters
               .slice(1)
-              .map((v) => (<ts.Identifier>v.name).text)
+              .map((v) => (<ts.Identifier>v.name).text);
             rtn2 += RPCApi(
               serverName,
               name,
               args.params,
               args.rtnType,
               args.call.toString(),
-              [...ns, `'${name}'`].toString()
-            )
-            break
+              [...dns, ...ns, `'${name}'`].toString()
+            );
+            break;
           case ts.SyntaxKind.ImportDeclaration:
-            break
+            break;
           default:
             console.log(
-              'nodes that are not parsed',
+              "nodes that are not parsed",
               node.kind,
               tsContent.slice(node.pos, node.end)
-            )
+            );
         }
-      })
-      return rtn2
-    }
+      });
+      return rtn2;
+    };
 
     sourceFile.forEachChild((node) => {
       // Parse each module declaration
       if (ts.SyntaxKind.ModuleDeclaration === node.kind) {
-        const Nd = <ts.ModuleDeclaration>node
-        rtn += moduleParse(Nd.body)
+        const Nd = <ts.ModuleDeclaration>node;
+        rtn += moduleParse(Nd.body);
       }
-    })
+    });
 
-    return rtn
+    return rtn;
   }
 }
 
@@ -144,30 +242,30 @@ export async function pkgProxy(imports: Import[]) {
   for (const v of imports) {
     // Find in node_modules
     if (v.from.match(/^\w+/)) {
-      let file
-      const dts = `node_modules/@types/${v.from}.d.ts`
-      const nodeDts = `node_modules/@types/node/${v.from}.d.ts`
-      const pkgJson = `node_modules/${v.from}/package.json`
+      let file;
+      const dts = `node_modules/@types/${v.from}.d.ts`;
+      const nodeDts = `node_modules/@types/node/${v.from}.d.ts`;
+      const pkgJson = `node_modules/${v.from}/package.json`;
       // Search the package itself first, then dts, and then nodeDts
       // Read the package.json file of the package
       if (fs.existsSync(pkgJson)) {
-        const pkg = require(path.resolve(pkgJson))
-        const types = pkg.types
+        const pkg = require(path.resolve(pkgJson));
+        const types = pkg.types;
         if (types) {
           file = fs.readFileSync(
             path.join(`node_modules/${v.from}`, types),
-            'utf-8'
-          )
+            "utf-8"
+          );
           await tsParse({
             baseDir: path.resolve(`node_modules/${v.from}`),
-            out: 'dts.d.ts',
+            out: "dts.d.ts",
             files: [types],
-          })
+          });
         }
       } else {
-        file = fs.existsSync(dts) && fs.readFileSync(dts, 'utf-8')
+        file = fs.existsSync(dts) && fs.readFileSync(dts, "utf-8");
         if (!file) {
-          file = fs.existsSync(nodeDts) && fs.readFileSync(nodeDts, 'utf-8')
+          file = fs.existsSync(nodeDts) && fs.readFileSync(nodeDts, "utf-8");
         }
       }
     } else {
@@ -176,11 +274,11 @@ export async function pkgProxy(imports: Import[]) {
 }
 
 export function extractor() {
-  const apiExtractorJsonPath: string = path.join('api-extractor.json')
+  const apiExtractorJsonPath: string = path.join("api-extractor.json");
 
   // Load and parse the api-extractor.json file
   const extractorConfig: ExtractorConfig =
-    ExtractorConfig.loadFileAndPrepare(apiExtractorJsonPath)
+    ExtractorConfig.loadFileAndPrepare(apiExtractorJsonPath);
 
   // Invoke API Extractor
   const extractorResult: ExtractorResult = Extractor.invoke(extractorConfig, {
@@ -189,16 +287,16 @@ export function extractor() {
 
     // Equivalent to the "--verbose" command-line parameter
     showVerboseMessages: true,
-  })
+  });
 
   if (extractorResult.succeeded) {
-    console.log(`API Extractor completed successfully`)
-    process.exitCode = 0
+    console.log(`API Extractor completed successfully`);
+    process.exitCode = 0;
   } else {
     console.error(
       `API Extractor completed with ${extractorResult.errorCount} errors` +
         ` and ${extractorResult.warningCount} warnings`
-    )
-    process.exitCode = 1
+    );
+    process.exitCode = 1;
   }
 }
